50ed65a8b020c497fd5ab62382388667f35b8a84
Rahul Raj <rahulr7177@gmail.com>
2024-09-11 23:22:00 +0530

    Update home.jsx

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import profileimg from '../Assets/pfp.png'
-
+import React from 'react';
+import profileimg from '../Assets/profilepic.png';
 
 const Home = () => {
-  return (
-    <div name="home" className="h-screen w-full items-center bg-gradient-to-b from-black via-black to-gray-800 text-white">
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-            <div className="flex flex-col justify-center h-full sd:items-center">
-                <h2 className="text-4xl sm:text-7xl font-bold text-blue-500">RAHUL RAJ</h2>
-                <p className="text-gray-300 py-4 w-fit text-xl">
-                    Aspiring Software Developer<span className=' font-bold text-xl'> | </span> <br />
-                    Designing and coding beautiful experiences.
-                </p>
-            </div>
-        <div>
-            <img src={profileimg} alt="Profile Img" className="rounded-2xl mx-auto w-2/3 md:w-2/3 " />
-        </div>
-        </div>
-    </div>
-  )
-}
+  return (
+    <div
+      name="home"
+      className="h-screen w-full items-center bg-gradient-to-b from-black via-black to-gray-800 text-white"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-between h-full px-4">
+        <div className="flex flex-col justify-center h-full text-center md:text-left">
+          <h2 className="text-4xl sm:text-7xl font-bold text-blue-500">RAHUL RAJ</h2>
+          <p className="text-gray-300 py-4 w-fit text-xl">
+            Aspiring Software Developer<span className="font-bold text-xl"> | </span> <br />
+            Designing and coding beautiful experiences.
+          </p>
+        </div>
+        <div className="flex justify-center items-center w-full md:w-auto mt-4 md:mt-0">
+          <img
+            src={profileimg}
+            alt="Profile Img"
+            className="rounded-full mx-auto w-64 md:w-96  mb-16" // Increased size
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
+
