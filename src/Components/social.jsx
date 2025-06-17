9c7be08f5cc1aef50f0b5d013ea8335153610983
Rahul Raj <rahulr7177@gmail.com>
2025-06-17 15:53:01 +0530

    Update social.jsx
    Added updated resume

--- src/Components/social.jsx
+++ src/Components/social.jsx
@@ -25,7 +25,7 @@ const Social = () => {
                 </a>
             </li>
             <li className="flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 rounded-br">
-                <a href="https://drive.google.com/file/d/1B6UhHQCxPvdsh35660hF4fWx9IeP63lA/view?usp=drive_link" className="flex justify-between items-center w-full text-white">
+                <a href="https://drive.google.com/file/d/1r30CJyESJOlaGVMANaXGXgODAXaggqaf/view?usp=drive_link" className="flex justify-between items-center w-full text-white">
                 <>Resume <BsFillPersonLinesFill size={30}/>
                 </>
                 </a>
